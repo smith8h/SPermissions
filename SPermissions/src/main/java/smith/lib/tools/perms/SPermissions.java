fc8bbf580f0492c637bb265af4f77263fd9a5423
Dev. Smith <husseinshakir81@gmail.com>
2024-04-01 05:36:19 +0300

    feat: add storage access request and checking for android 11+ and 10-

--- SPermissions/src/main/java/smith/lib/tools/perms/SPermissions.java
+++ SPermissions/src/main/java/smith/lib/tools/perms/SPermissions.java
@@ -2,8 +2,15 @@ package smith.lib.tools.perms;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.PackageManager;
-
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.Settings;
+import androidx.activity.ComponentActivity;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -13,10 +20,12 @@ import androidx.core.content.ContextCompat;
  * way! ask for permissions at one or at a single request, and check weather your app has access to
  * that features.
  */
-@SuppressWarnings({"unused"})
-public class SPermissions {
+@SuppressWarnings({"unused", "deprecation"})
+public class SPermissions extends ComponentActivity {
 
     private final Activity activity;
+    private SPermissionsCallback callback;
+    private int requestCode = -1;
 
     /**
      * The static request code declared by <b>{@code SPermissions}</b> module.
@@ -64,7 +73,7 @@ public class SPermissions {
      * permission you need in a convenient way.
      * @param permission the required permission to be granted.
      */
-    public void requestPermission(@NonNull String permission) {
+    public void askPermission(@NonNull String permission) {
         ActivityCompat.requestPermissions(activity, new String[]{permission}, REQUEST_CODE);
     }
 
@@ -73,7 +82,7 @@ public class SPermissions {
      * permissions you need in a convenient way at once.
      * @param permissions the required permissions to be granted as string array.
      */
-    public void requestPermissions(@NonNull String[] permissions) {
+    public void askPermissions(@NonNull String[] permissions) {
         ActivityCompat.requestPermissions(activity, permissions, REQUEST_CODE);
     }
 
@@ -83,7 +92,7 @@ public class SPermissions {
      * @param permission the required permission to be granted.
      * @param requestCode the code to distinguish multiple permission grant access requests.
      */
-    public void requestPermission(@NonNull String permission, int requestCode) {
+    public void askPermission(@NonNull String permission, int requestCode) {
         ActivityCompat.requestPermissions(activity, new String[]{permission}, requestCode);
     }
 
@@ -93,7 +102,87 @@ public class SPermissions {
      * @param permissions the required permissions to be granted as string array.
      * @param requestCode the code to distinguish multiple permission grant access requests.
      */
-    public void requestPermissions(@NonNull String[] permissions, int requestCode) {
+    public void askPermissions(@NonNull String[] permissions, int requestCode) {
         ActivityCompat.requestPermissions(activity, permissions, requestCode);
+    }
+
+    /**
+     * Make a request to get granted to access Reading and Writing in external storage features in
+     * the device with this method, ask for permissions {@link  Manifest.permission#READ_EXTERNAL_STORAGE}
+     * and {@link  Manifest.permission#WRITE_EXTERNAL_STORAGE} for android -10 and below, and {@code Scoped Storage Access} for android
+     * +11 and above.
+     *
+     * @param callback the callback to be called when the permissions are granted or not.
+     */
+    public void askStorageAccess(SPermissionsCallback callback) {
+        this.callback = callback;
+        askStorage(-1);
+    }
+
+    /**
+     * Make a request to get granted to access Reading and Writing in external storage features in
+     * the device with this method, ask for permissions {@link  Manifest.permission#READ_EXTERNAL_STORAGE}
+     * and {@link  Manifest.permission#WRITE_EXTERNAL_STORAGE} for android -10 and below, and {@code Scoped Storage Access} for android
+     * +11 and above.
+     *
+     * @param callback the callback to be called when the permissions are granted or not.
+     * @param requestCode the unique identifier of the request being asked.
+     */
+    public void askStorageAccess(SPermissionsCallback callback, int requestCode) {
+        this.callback = callback;
+        askStorage(requestCode);
+    }
+
+    private final ActivityResultLauncher<Intent> storageResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), o -> {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            if (callback != null) this.callback.callback(Environment.isExternalStorageManager(),
+                    requestCode == -1 ? REQUEST_CODE : requestCode);
+        }
+    });
+
+    private void askStorage(int requestCode) {
+        this.requestCode = requestCode;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            Intent intent = new Intent();
+            try {
+                intent.setAction(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
+                intent.setData(Uri.fromParts("package", activity.getPackageName(), null));
+            } catch (Exception e) {
+                intent.setAction(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION);
+            }
+            storageResultLauncher.launch(intent);
+        } else {
+            askPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    requestCode == -1 ? REQUEST_CODE : requestCode);
+        }
+    }
+
+    /**
+     * Check weather your app has successful access to Reading and Writing in external storage features.
+     * Permissions like {@link Manifest.permission#READ_EXTERNAL_STORAGE}, {@link Manifest.permission#WRITE_EXTERNAL_STORAGE}.
+     * and {@link Environment#isExternalStorageManager()}.
+     *
+     * @return true if access to external storage is granted.
+     */
+    public boolean hasStorageAccess() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            return Environment.isExternalStorageManager();
+        } else {
+            return hasPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE});
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == SPermissions.REQUEST_CODE || requestCode == this.requestCode) {
+            if (grantResults.length > 0) {
+                if (hasPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE})) {
+                    if (callback != null) callback.callback(true, requestCode);
+                }
+            }
+        } else {
+            if (callback != null) callback.callback(false, -1);
+        }
     }
 }
---
+++ SPermissions/src/main/java/smith/lib/tools/perms/SPermissionsCallback.java
@@ -0,0 +1,13 @@
+package smith.lib.tools.perms;
+
+public interface SPermissionsCallback {
+
+    /**
+     * Called when the permissions are changed for the application.
+     *
+     * @param isGranted   true if the permissions are granted.
+     * @param requestCode the request code that has been sent with the ask method to distinguish
+     *                    between permissions requests.
+     */
+    void callback(boolean isGranted, int requestCode);
+}
